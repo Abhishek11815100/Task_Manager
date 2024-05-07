589312b3e5c35ac1ad561b9f8cd1aa118de3ebf6
Abhishek11815100 <abhiyadav5100@gmail.com>
2024-05-07 15:06:49 +0530

    test

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use('/',profileRoutes)
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.resolve(__dirname, "../frontend/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend/build/index.html")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../client/build/index.html")));
 }
 
 const PORT = process.env.PORT || 8000;
