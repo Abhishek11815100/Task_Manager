bee89a4ea05d0ed857cc339be68da3c44f149196
Abhishek11815100 <abhiyadav5100@gmail.com>
2024-05-07 15:25:30 +0530

    prod fix

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use('/',authRoutes);
 app.use('/',profileRoutes)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "../frontend/build")));
+    app.use(express.static(path.resolve(__dirname, "../client/build")));
     app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../client/build/index.html")));
 }
 
