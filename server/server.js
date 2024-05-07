40217e5750e98a2d740441c72457780542059a14
Abhishek11815100 <abhiyadav5100@gmail.com>
2024-05-07 13:44:15 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://task-manager-f.onrender.com',
     credentials: true,
 }));
 
